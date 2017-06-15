be4e2a0ee5a86563a08195cc957aa24a96179353
mldn <wolf_hk@sina.com>
2017-06-15 10:28:07 +0800

    In Hello.java add two statement

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
-public class hello {
-	public static void main(String args[]) {
-		System.out.println("Hello GIT");
+
+public class Hello {
+	public static void main(String[] args) {
+		//System.out.println("Hello Wold!");
+		System.out.println("Hello Git!");
+		System.out.println("天天开心！");
+		System.out.println("永远快乐！");
 	}
 }
