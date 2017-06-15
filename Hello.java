28c66e6fd9299e873bb774a1f1a667beed4d5053
mldn <wolf_hk@sina.com>
2017-06-15 11:18:26 +0800

    Commit work dir file

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
-
 public class Hello {
 	public static void main(String[] args) {
 		//System.out.println("Hello Wold!");
 		System.out.println("Hello Git!");
 		System.out.println("天天开心！");
 		System.out.println("永远快乐！");
+		System.out.println("世界你好！");
 	}
 }
