e75a0646c8ec934eb053db3339e5d29b6e07a9c4
mldn <wolf_hk@sina.com>
2017-06-19 21:51:13 +0800

    Change Hello.java in dev branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,8 @@
+import java.util.*
 public class Hello {
 	public static void main(String[] args) {
 		//System.out.println("Hello Wold!");
 		System.out.println("Hello Git!");
-		System.out.println("天天开心！");
-		System.out.println("永远快乐！");
-		System.out.println("世界你好！");
+		Date date = new Date();
 	}
 }
